70f373141eb9bb056586699c05671779617b38db
Abduvohid123 <abduvohid.mirzamahmudov@gmail.com>
2023-02-12 02:34:55 +0500

    test

--- app/Telegram/Commands/StartCommand.php
+++ app/Telegram/Commands/StartCommand.php
@@ -5,20 +5,23 @@ namespace App\Telegram\Commands;
 class StartCommand
 {
 
-    public static function handle(\TelegramBot\Api\Types\Message $message)
+    public static function handle($bot)
     {
-        $bot = new \TelegramBot\Api\Client('6022814437:AAGKJ8NsTWGxmENsZ3KcnE1YqA1RZKmCurw');
 
-        try {
+        return \Closure::fromCallable(function (\TelegramBot\Api\Types\Message $message) use ($bot){
+
+            try {
 
 
-            $chatId = $message->getChat()->getId();
-            $link = new \TelegramBot\Api\Types\Inline\InlineKeyboardMarkup([[['text' => "O'zbek", 'callback_data' => "uz"], ['text' => 'English', 'callback_data' => "eng"]], [['text' => 'Русский', 'callback_data' => "ru"]]]);
-            $bot->sendMessage($chatId, "<b>🇺🇿 Iltimos tilni tanlang!\n\n🇬🇧 Please! choose a language!\n\n🇷🇺 Пожалуйста, выберите язык!</b>", "HTML", false, null, $link);
+                $chatId = $message->getChat()->getId();
+                $link = new \TelegramBot\Api\Types\Inline\InlineKeyboardMarkup([[['text' => "O'zbek", 'callback_data' => "uz"], ['text' => 'English', 'callback_data' => "eng"]], [['text' => 'Русский', 'callback_data' => "ru"]]]);
+                $bot->sendMessage($chatId, "<b>🇺🇿 Iltimos tilni tanlang!\n\n🇬🇧 Please! choose a language!\n\n🇷🇺 Пожалуйста, выберите язык!</b>", "HTML", false, null, $link);
 
-            $bot->run();
-        } catch (Exception $exception) {
 
-        }
+            } catch (Exception $exception) {
+
+            }
+        });
+
     }
 }
