7f0065c33617642adc020c31944f49ac4215f526
Abduvohid123 <abduvohid.mirzamahmudov@gmail.com>
2023-02-11 19:08:55 +0500

    test

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Telegram\Bot\Api;
 
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/telegram', function () {
+    $telegram = new Api('6022814437:AAGKJ8NsTWGxmENsZ3KcnE1YqA1RZKmCurw');
+    return  $telegram->getMe();
 });
