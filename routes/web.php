d9a52734d7955ff3d8c2b80ec6887cff84e55737
Abduvohid123 <abduvohid.mirzamahmudov@gmail.com>
2023-02-12 01:12:04 +0500

    test

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::post('/6022814437:AAGKJ8NsTWGxmENsZ3KcnE1YqA1RZKmCurw/webhook', function 
 Route::get('/telegram', function () {
     $telegram = new Api('6022814437:AAGKJ8NsTWGxmENsZ3KcnE1YqA1RZKmCurw');
     $telegram->setWebhook([
-        'url'=>'http://bot.byweb.uz'
+        'url'=>'https://bot.byweb.uz'
     ]);
 
 });
