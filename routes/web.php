6560fecd324c83d1708c7ab10d2187bb8b898a19
Abduvohid123 <abduvohid.mirzamahmudov@gmail.com>
2023-02-12 01:05:04 +0500

    test

--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,6 @@ Route::post('/6022814437:AAGKJ8NsTWGxmENsZ3KcnE1YqA1RZKmCurw/webhook', function 
 
 Route::get('/telegram', function () {
     $telegram = new Api('6022814437:AAGKJ8NsTWGxmENsZ3KcnE1YqA1RZKmCurw');
-    $telegram->sendMessage([
-       'chat_id'=>534310866,
-       'text'=>"salom"
-    ]);
+    $telegram->deleteWebhook();
+   dd($telegram->getUpdates());
 });
