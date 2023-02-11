b87d21b816462190bba27781dc924ae9c1f8f880
Abduvohid123 <abduvohid.mirzamahmudov@gmail.com>
2023-02-12 02:06:48 +0500

    test

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,25 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Telegram\Bot\Api;
-use Telegram\Bot\Laravel\Facades\Telegram;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+
 Route::post('/6022814437:AAGKJ8NsTWGxmENsZ3KcnE1YqA1RZKmCurw/webhook', function () {
-    $telegram = new Api('6022814437:AAGKJ8NsTWGxmENsZ3KcnE1YqA1RZKmCurw');
 
 
 
 })->name('webhook');
-
-
-Route::get('/telegram', function () {
-    $telegram = new Api('6022814437:AAGKJ8NsTWGxmENsZ3KcnE1YqA1RZKmCurw');
-
-
-});
